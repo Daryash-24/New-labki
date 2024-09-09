c4a3bceacd02e9cd7320fab450148dcfcfda8ba3
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-09-09 09:23:07 +0300

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask "
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Дыбалина Дарья Александровна, Лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Дарья Дыбалина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
