46733d0ac58b160bb9e8145863dde060b586bdde
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-10-01 22:11:32 +0300

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -280,5 +280,8 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    name = 'Дарья Дыбалинa'
-    return render_template('example.html', name = name)
+    name = 'Дарья Дыбалинa';
+    number = '2';
+    group = 'ФБИ-23';
+    cours = '3'
+    return render_template('example.html', name = name, number = number, group = group, cours = cours)
