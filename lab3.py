8cd3a3c0bef5a278f5d5f2fa7ad5a59ffa486c68
Darya Dybalina <dasha.dybalina1@gmail.com>
2024-10-16 16:43:02 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, template_rendered, render_template, request, make_response, redirect
+from flask import Blueprint, template_rendered, render_template, request, make_response, redirect, session
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
@@ -36,6 +36,35 @@ def form1():
     age = request.args.get('age')
     if age == '':
         errors['age'] = 'Поле не заполнено!'
-        
+
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template ('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffe':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template ('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', 0)
+    return render_template ('lab3/success.html', price=price)
