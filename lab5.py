aced847fdf80e608779ddf2f5bab4c2f76c9c59f
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-11-13 22:00:57 +0300

    Лаба 5

---
+++ lab5.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, url_for, redirect, render_template, request, session
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    return render_template('/lab5/lab5.html')
+
+
