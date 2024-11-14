2cf5348e28d314926798e3fdf42ea5c5eb305d05
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-11-14 19:03:58 +0300

    Регистрация

--- lab5.py
+++ lab5.py
@@ -18,35 +18,23 @@ def register():
     if not (login and password):
         return render_template('lab5/register.html', error='Заполните все поля')
 
-    conn = None
-    cur = None
+    conn = psycopg2.connect(dbname="webka", user="postgres", password="postgres", host="127.0.0.1", options="-c client_encoding=UTF8")
+    cur = conn.cursor()
 
-    try:
-        conn = psycopg2.connect(dbname="webka", user="postgres", password="postgres", host="127.0.0.1", options="-c client_encoding=UTF8")
-        cur = conn.cursor()
+  # Используйте параметризованный запрос для проверки наличия пользователя
+    cur.execute("SELECT login FROM users WHERE login=%s", (login,))
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html', error = 'Такой пользователь уже существует')
 
-        # Используйте параметризацию запросов для предотвращения SQL-инъекций
-        cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
-        if cur.fetchone():
-            return render_template('lab5/register.html', error='Такой пользователь уже существует')
+  # Используйте параметризованный запрос для вставки данных
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s)", (login, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login = login)
 
-        # Хешируйте пароль перед сохранением
-        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login))
-        conn.commit()
-
-    except Exception as e:
-        # Логирование ошибки
-        print(f"Error: {e}")
-        return render_template('lab5/register.html', error='Произошла ошибка при регистрации. Попробуйте позже.')
-
-    finally:
-        # Закрываем соединение с базой данных, если оно было открыто
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
-
-    return render_template('lab5/success.html', login=login)
 
 @lab5.route('/lab5/success')
 def success():
