8655f908e9ae62aa7dd97d99d11de69f885c9387
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-11-14 23:40:38 +0300

    надо

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, render_template_string, abort, request, session
+from flask import Flask, redirect, url_for, render_template, render_template_string, abort, request, session, os
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
