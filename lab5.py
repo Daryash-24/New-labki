085fd035da4ade9a6d72937f3fb56438186f579e
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-11-14 22:36:46 +0300

    Исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -67,7 +67,7 @@ def login():
     conn, cur = db_connect()
     cur = conn.cursor(cursor_factory = RealDictCursor)
 
-    cur.execute(f"SELECT * FROM users WHERE login ='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
     user = cur.fetchone()
 
     if not user:
