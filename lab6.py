e111f045d9b4290735f5298b247c9d4ef3de2b0f
Darya Dybalina <dasha.dybalina1@gmail.com>
2024-11-28 16:29:26 +0700

    Заготовка к 6-ой лабораторной

---
+++ lab6.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, url_for, redirect, render_template, request, session, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from os import path
+
+lab6 = Blueprint('lab6', __name__)
+
+@lab6.route('/lab6/')
+def lab():
+    return render_template('lab6/lab6.html')
+
