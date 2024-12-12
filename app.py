c5608b8e4de16757344c6896dde332c9c53de272
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-12-12 15:13:27 +0300

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 from rgz2 import rgz2
 
 app = Flask(__name__)
@@ -15,6 +16,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 app.register_blueprint(rgz2)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'сила в любви')
@@ -49,6 +51,7 @@ def menu():
             <li><a href="/lab4">Четвертая лабораторная</a></li>
             <li><a href="/lab5">Пятая лабораторная</a></li>
             <li><a href="/lab6">Шестая лабораторная</a></li>
+            <li><a href="/lab7">Седьмая лабораторная</a></li>
             <li><a href="/rgz2">StorageHub.Камера Хранения</a></li>
             
         </ol>
