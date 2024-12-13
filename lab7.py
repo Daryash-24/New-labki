f705000b75001c6bd4441e0500ce5571974150b6
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-12-13 18:42:43 +0300

    jsonify

--- lab7.py
+++ lab7.py
@@ -1,36 +1,10 @@
-from flask import Blueprint, redirect, url_for, render_template, render_template_string, abort, request, make_response, session,  current_app
-from functools import wraps
+from flask import Blueprint, redirect, url_for, render_template, render_template_string, abort, request, make_response, jsonify,  current_app
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from os import path
 
 lab7 = Blueprint('lab7', __name__, static_folder='static')
-
-def db_connect():
-
-    if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(dbname="webka", 
-        user="postgres", 
-        password="postgres", 
-        host="127.0.0.1"
-        )
-        cur = conn.cursor(cursor_factory = RealDictCursor)
-
-    else:
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-
-    return conn, cur
-
-def db_close(conn, cur):
-    conn.commit()
-    cur.close()
-    conn.close()
 
 @lab7.route('/lab7/')
 def main():
@@ -89,7 +63,7 @@ films = [
 
 @lab7.route('/lab7/rest-api/films', methods=['GET'])
 def get_films():
-    return films
+    return jsonify (films)
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
