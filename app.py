170b525caabcaf17e4c2d81f3ac606eaf509caf0
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-12-23 22:46:11 +0300

    Опять траблы

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, render_template_string, abort, request, session
 import os
+from os import path
+from db import db
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -23,6 +27,24 @@ app.register_blueprint(rgz2)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'сила в любви')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+db = SQLAlchemy()
+
+if app.config['DB_TYPE'] == 'postgresql':
+    db_name = 'darya_dybalina_orm'
+    db_user = 'darya_dybalina_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'darya_dybalina_orm.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 
 @app.route("/")
