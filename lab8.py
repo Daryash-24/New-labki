fb48d99924800a55bf08d08516b65d82dd363881
Дарья Дыбалина <dasha.dybalina1@gmail.com>
2024-12-25 12:58:26 +0300

    Авторизация

--- lab8.py
+++ lab8.py
@@ -5,14 +5,15 @@ import sqlite3
 from os import path
 from db import db  
 from db.models import users, article
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__, static_folder='static')
 
 
 @lab8.route('/lab8/')
 def main():
-    return render_template('/lab8/lab8.html', user = session.get('login'))
+    return render_template('/lab8/lab8.html', user=current_user.login if current_user.is_authenticated else None)
 
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
@@ -42,3 +43,26 @@ def register():
     db.session.commit()
     
     return redirect('/lab8/')
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember=False)
+            return redirect('/lab8/')
+        
+    return render_template ('/lab8/login.html',
+                            error = 'Ошибка входа: логин и/или пароль неверны ')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
